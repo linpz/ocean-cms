f3d5e34c6828cd2c3de862d2401353a7bf67bc7e
linpz <e606@qq.com>
2021-08-14 15:16:57 +0800

    common

---
+++ ocean-cms-user/src/main/java/ocean/cms/user/config/ShiroConfig.java
@@ -0,0 +1,64 @@
+package ocean.cms.user.config;
+
+import ocean.cms.user.shiro.CustomAuthorizingRealm;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfig {
+
+    @Bean
+    public HashedCredentialsMatcher hashedCredentialsMatcher() {
+        HashedCredentialsMatcher credentialsMatcher = new HashedCredentialsMatcher();
+        credentialsMatcher.setHashAlgorithmName("md5");
+        credentialsMatcher.setHashIterations(2);
+        credentialsMatcher.setStoredCredentialsHexEncoded(true);
+        return credentialsMatcher;
+    }
+
+    @Bean
+    public CustomAuthorizingRealm customAuthorizingRealm() {
+        CustomAuthorizingRealm customAuthorizingRealm = new CustomAuthorizingRealm();
+        customAuthorizingRealm.setCredentialsMatcher(hashedCredentialsMatcher());
+        return customAuthorizingRealm;
+    }
+
+    @Bean
+    public SecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(customAuthorizingRealm());
+        return securityManager;
+    }
+
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        Map<String, String> filterMap = new HashMap<String, String>();
+        filterMap.put("/logout", "logout");
+        filterMap.put("/swagger**/**", "anon");
+        filterMap.put("/webjars/**", "anon");
+        filterMap.put("/v2/**", "anon");
+        filterMap.put("/**", "authc");
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        shiroFilterFactoryBean.setSuccessUrl("/index");
+        shiroFilterFactoryBean.setUnauthorizedUrl("/error");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterMap);
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+        return authorizationAttributeSourceAdvisor;
+    }
+}
---
+++ ocean-cms-user/src/main/java/ocean/cms/user/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package ocean.cms.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String name;
+    private String password;
+}
